64bb8cea17a2f947118b7003439b832d310c39ba
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-08-25 16:43:47 +0200

    new elements have been added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    
+        <?php
+             $myParagraph = 'Esattamente come JavaScript, anche PHP è un tipo di linguaggio Case Sensitive,
+             quindi facciamo attenzione a maiuscole e minuscole nei nomi delle variabili e delle funzioni.
+             Se abbiamo un nome composto possiamo scegliere uno dei due stili: underscore $mia_variabile oppure camelCase $miaVariabile
+             Attenzione: spazi e - non sono ammessi!!!<br/>';
+             
+            echo $myParagraph;
+            $length = strlen($myParagraph);
+            echo "La lunghezza ha <b>{$length} </b>caratteri<br/>";
+            $wordToSearch = $_GET['word'];
+            
+        ?>
+    
+    </main>
+</body>
+</html>
